dd8a79d9bf43bc64eae98d11d25dd58a456c5185
HoyaRobotics <hhs4152@gmail.com>
2024-05-23 13:48:04 -0400

    Allison 23-2

--- src/main/java/frc/robot/generated/ShooterConstants.java
+++ src/main/java/frc/robot/generated/ShooterConstants.java
@@ -4,5 +4,46 @@
 
 package frc.robot.generated;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.VoltageConfigs;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+
 /** Add your docs here. */
-public class ShooterConstants {}
+public class ShooterConstants {     
+    public static final double gearRatio = 1.25;
+    public static final Slot0Configs shooterSlot0Configs = new Slot0Configs()
+        .withKS(0.27) // output to overcome static friction (output)
+        .withKV(0.1538) // output per unit of target velocity (output/rps)
+        .withKP(0.75) // output per unit of error in position (output/rotation)
+        .withKI(0.0) // output per unit of integrated error in position (output/(rotation*s))
+        .withKD(0.0); // output per unit of error in velocity (output/rps)
+    public static final CurrentLimitsConfigs shooterCurrentLimits = new CurrentLimitsConfigs()
+        .withStatorCurrentLimit(45.0)
+        .withStatorCurrentLimitEnable(true);
+    public static final VoltageConfigs shooterVoltageConfigs = new VoltageConfigs()
+        .withPeakForwardVoltage(11.0)
+        .withPeakReverseVoltage(11.0);
+    public static final FeedbackConfigs shooterFeedbackConfigs = new FeedbackConfigs()
+        .withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor)
+        .withSensorToMechanismRatio(gearRatio);
+
+    /*public static final double kP = 0.0001;
+    public static final double kI = 0.0;
+    public static final double kD = 0.0002;
+    public static final double kFF = 0.000228;*/
+
+    public static final double shootingRPM = 4200; //4200 ideal
+    //public static final double spinFactor = 0.05; //0.1
+    public static final double speedThreshold = 0.98;
+    //public static final double idleSpeed = 2000;
+
+    public static final double shootPosition = 112;
+
+    public static final double xSetPoint = 0;
+    public static final double ySetPoint = 0;
+    
+    public static final int leftShooterMotorID = 24;
+    public static final int rightShooterMotorID = 25;
+}
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,14 +4,67 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.generated.ShooterConstants;
+//import monologue.LogLevel;
+//import monologue.Logged;
+//import monologue.Annotations.Log;
 
 public class Shooter extends SubsystemBase {
+  
+  private TalonFX shootLeft = new TalonFX(ShooterConstants.leftShooterMotorID);
+  private TalonFX shootRight = new TalonFX(ShooterConstants.rightShooterMotorID);
+
+  final VelocityVoltage voltageRequest = new VelocityVoltage(0);
+  final PowerDistribution pdp = new PowerDistribution (1, ModuleType.kRev);
+
+  //private boolean upToSpeed = false;
+
+  double leftShooterSpeed;
+  double rightShooterSpeed;
+
+  /*@Log.File(level = LogLevel.DEFAULT) double leftShooterSpeed;
+  @Log.File(level = LogLevel.DEFAULT) double rightShooterSpeed;
+  @Log.File(level = LogLevel.DEFAULT) double leftShooterSetpoint;
+  @Log.File(level = LogLevel.DEFAULT) double rightShooterSetpoint;
+
   /** Creates a new Shooter. */
-  public Shooter() {}
+  public Shooter() {
+    configShooterMotors();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    leftShooterSpeed = shootLeft.getVelocity().getValueAsDouble();
+    rightShooterSpeed = shootRight.getVelocity().getValueAsDouble();
   }
+
+  private void configShooterMotors(){
+    shootRight.getConfigurator().apply(new TalonFXConfiguration());
+    shootLeft.getConfigurator().apply(new TalonFXConfiguration());
+    var talonfxConfigs = new TalonFXConfiguration();
+    talonfxConfigs.Slot0 = ShooterConstants.shooterSlot0Configs;
+    talonfxConfigs.CurrentLimits = ShooterConstants.shooterCurrentLimits;
+    talonfxConfigs.Voltage = ShooterConstants.shooterVoltageConfigs;
+    talonfxConfigs.Feedback = ShooterConstants.shooterFeedbackConfigs;
+    talonfxConfigs.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    shootLeft.getConfigurator().apply(talonfxConfigs);
+    shootRight.getConfigurator().apply(talonfxConfigs);
+    shootRight.setInverted(true);
+  }
+
+  public void setShooterSpeeds(){
+    //shootLeft.setControl(voltageRequest.withVelocity(ShooterConstants.shootingRPM*(1-ShooterConstants.spinFactor)/60));
+    //shootRight.setControl(voltageRequest.withVelocity(ShooterConstants.shootingRPM*(1+ShooterConstants.spinFactor)/60));
+  }
+
 }
