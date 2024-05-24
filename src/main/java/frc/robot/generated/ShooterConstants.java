b9bbffca3574970834071f70635f5530f24f5686
HoyaRobotics <hhs4152@gmail.com>
2024-05-24 13:17:16 -0400

    Shooter 24

--- src/main/java/frc/robot/generated/ShooterConstants.java
+++ src/main/java/frc/robot/generated/ShooterConstants.java
@@ -34,8 +34,8 @@ public class ShooterConstants {
     public static final double kD = 0.0002;
     public static final double kFF = 0.000228;*/
 
-    public static final double shootingRPM = 4200; //4200 ideal
-    //public static final double spinFactor = 0.05; //0.1
+    public static final double shootingRPM = 500; //4200 ideal
+    public static final double spinFactor = 0.05; //0.1
     public static final double speedThreshold = 0.98;
     //public static final double idleSpeed = 2000;
 
