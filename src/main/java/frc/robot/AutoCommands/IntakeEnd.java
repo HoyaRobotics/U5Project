10065f8649146bb561e55e3b551e6fa7c51c7c6d
HoyaRobotics <hhs4152@gmail.com>
2024-06-10 09:00:37 -0400

    Sophia 6/10

---
+++ src/main/java/frc/robot/AutoCommands/IntakeEnd.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.AutoCommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+import frc.robot.generated.IntakeConstants;
+
+public class IntakeEnd extends Command {
+  Intake intake;
+
+  /** Creates a new IntakeStart. */
+  public IntakeEnd(Intake intake) {
+    this.intake = intake;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    intake.setIntakePosition(IntakeConstants.homePosition);
+    intake.setRollerSpeed(IntakeConstants.stallSpeed);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
